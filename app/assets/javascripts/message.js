728343b80d904568ae773aa87dd707a876c3ebfa
shounosukeiko <toshimichi.fukushima@gmail.com>
2020-02-04 21:26:06 +0900

    initial

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,56 @@
 $(function(){ 
-  function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-box">
-           <div class="upper-box__name">
-             ${message.user_name}
-           </div>
-           <div class="upper-box__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-box">
-           <p class="lower-box__text">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-box">
-           <div class="upper-box__name">
-             ${message.user_name}
-           </div>
-           <div class="upper-box__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-box">
-           <p class="lower-box__text">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
-  }  
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+          `<div class="upper-box">` +
+            `<div class="upper-box__name">` +
+              message.user_name +
+            `</div>` +
+            `<div class="upper-box__date">` +
+              message.created_at +
+            `</div>` +
+          `</div>` +
+          `<div class="lower-box">` +
+            `<p class="lower-box__text">` +
+              message.content +
+            `</p>` +
+            `<img src="` + message.image + `" class="lower-box__image" >` +
+          `</div>` +
+        `</div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-box">` +
+          `<div class="upper-box__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-box__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-box">` +
+          `<p class="lower-box__text">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-box">` +
+          `<div class="upper-box__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-box__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-box">` +
+          `<img src="` + message.image + `" class="lower-box__image" >` +
+        `</div>` +
+      `</div>`
+    };
+    return html;
+  };  
+  
   $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -64,4 +76,30 @@ $(function(){
     $('.form__submit').prop('disabled', false);
   });
   })
+  
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
